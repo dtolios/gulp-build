6f0e857fd1f87fae7714f4ffb107fad512cb1324
Dimitri Tolios <dtolios@riotgames.com>
2017-06-28 18:06:32 -0700

    Added sass compiling

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 
@@ -12,24 +13,36 @@ gulp.task('default', function () {
 });
 
 gulp.task('concatScripts', function () {
-    gulp.src([
-        'js/global.js',
-        'js/circle/autogrow.js',
-        'js/circle/circle.js'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('all.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('js'));
+    return gulp.src('js/**/*.js')
+                .pipe(sourcemaps.init())
+                .pipe(concat('all.js'))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('minifyScript', function () {
-    gulp.src('js/all.js')
-    .pipe(uglify())
-    .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('dist/scripts'));
+gulp.task('minifyScripts', function () {
+    return gulp.src('dist/scripts/all.js')
+                .pipe(sourcemaps.init())
+                .pipe(uglify())
+                .pipe(rename('all.min.js'))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('compileSass', function () {
+    return gulp.src('sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('clean', function () {
     del('dist');
 });
+
+// [
+//     'js/global.js',
+//     'js/circle/autogrow.js',
+//     'js/circle/circle.js'
+// ]
