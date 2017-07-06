4e723eec12e8ec0fa1caed9724a0663ea7afc452
Dimitri Tolios <dtolios@riotgames.com>
2017-07-05 18:19:49 -0700

    more work towards completion

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,73 @@
 'use strict';
 
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const gulp       = require('gulp');
+const concat     = require('gulp-concat');
+const rename     = require('gulp-rename');
+const uglify     = require('gulp-uglify');
+const sass       = require('gulp-sass');
+const cleanCSS   = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-
-gulp.task('default', function () {
-    // place code for your default task here
-});
+const imagemin   = require('gulp-imagemin');
+const del        = require('del');
 
 gulp.task('concatScripts', function () {
-    return gulp.src('js/**/*.js')
+    return gulp.src('./js/**/*.js')
                 .pipe(sourcemaps.init())
                 .pipe(concat('all.js'))
                 .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest('dist/scripts'));
+                .pipe(gulp.dest('js'));
 });
 
-gulp.task('minifyScripts', function () {
-    return gulp.src('dist/scripts/all.js')
-                .pipe(sourcemaps.init())
+gulp.task('minifyScripts', ['concatScripts'], function () {
+    return gulp.src('./js/all.js')
                 .pipe(uglify())
                 .pipe(rename('all.min.js'))
-                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('js'));
+});
+
+gulp.task('scripts', ['minifyScripts'], function () {
+    return gulp.src('./js/all.min.js')
                 .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('compileSass', function () {
-    return gulp.src('sass/**/*.scss')
+    return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minifyCss', ['compileSass'], function () {
+    return gulp.src('./css/global.css')
+                .pipe(cleanCSS({compatibility: 'ie8'}))
+                .pipe(rename('all.min.css'))
+                .pipe(gulp.dest('./css'));
+});
+
+gulp.task('styles', ['minifyCss'], function () {
+    return gulp.src('./css/all.min.css')
+                .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('./images/*')
+                .pipe(imagemin([
+                        imagemin.jpegtran({ progressive: true }),
+                        imagemin.optipng({ optimizationLevel: 5 })
+                ]))
+                .pipe(gulp.dest('./dist/content'));
 });
 
 gulp.task('clean', function () {
-    del('dist');
+    del(['dist', 'js/all*.js*', 'css']);
 });
 
-// [
-//     'js/global.js',
-//     'js/circle/autogrow.js',
-//     'js/circle/circle.js'
-// ]
+gulp.task('build', ['scripts', 'styles', 'images'], function () {
+    return gulp.src(['index.html'], {base: './'})
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
